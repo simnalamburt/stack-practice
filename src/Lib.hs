58af0897478d897d535bcb369a2ca6a6cb683504
Hyeon Kim <simnalamburt@gmail.com>
2015-12-26 19:44:14 +0900

    GuGuClass

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,11 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib ( someFunc ) where
+
+import Data.Foldable
+import Text.Printf
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  for_ [2..9] $ \(a :: Int) -> do
+    for_ [2..9] $ \b -> do
+      printf "%d * %d = %2d\n" a b (a*b)
+    putStr "\n"
