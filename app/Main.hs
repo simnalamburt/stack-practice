b4442bdb4f363191aaf7ee9e1e6793ee1fb75f34
Hyeon Kim <simnalamburt@gmail.com>
2021-12-13 07:43:08 +0900

    Implement a simple counter

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Web.Scotty (scotty, get, json, liftAndCatchIO)
 import Data.Aeson (object, (.=))
+import Control.Concurrent.STM.TVar (newTVarIO, readTVar, modifyTVar)
+import Control.Monad.STM (atomically)
 
 main :: IO ()
 main = do
     putStrLn "\nStarting web server, see http://localhost:3000 ...\n"
+    counter <- newTVarIO (0 :: Int)
     scotty 3000 $ do
         get "/" $ do
             -- Logging
-            liftAndCatchIO $ do
+            count <- liftAndCatchIO $ do
                 putStrLn "GET /"
-            let resp = object [ "count" .= (0 :: Int) ]
+                atomically $ do
+                    modifyTVar counter (+1)
+                    readTVar counter
+            let resp = object [ "count" .= count ]
             json resp
