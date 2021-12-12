f0666d0f3c5a066c5684bff0ebba5ae7c6e94e9e
Hyeon Kim <simnalamburt@gmail.com>
2021-12-13 05:28:45 +0900

    Simple web server

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,10 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-}
+import Web.Scotty (scotty, get, json)
+import Data.Text (Text)
 
 main :: IO ()
-main = putStrLn "Hello, world!"
+main = do
+    putStrLn "\nStarting web server, see http://localhost:3000 ...\n"
+    scotty 3000 $ do
+        get "/" $ do
+            json ("Hello, world!" :: Text)
