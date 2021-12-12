e466f2d8af4f8236c4b41e56ae8de9324da3109b
Hyeon Kim <simnalamburt@gmail.com>
2021-12-13 06:51:00 +0900

    liftAndCatchIO sample

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Web.Scotty (scotty, get, json)
+import Web.Scotty (scotty, get, json, liftAndCatchIO)
 import Data.Text (Text)
 
 main :: IO ()
@@ -7,4 +7,7 @@ main = do
     putStrLn "\nStarting web server, see http://localhost:3000 ...\n"
     scotty 3000 $ do
         get "/" $ do
+            -- Logging
+            liftAndCatchIO $ do
+                putStrLn "Hi!"
             json ("Hello, world!" :: Text)
